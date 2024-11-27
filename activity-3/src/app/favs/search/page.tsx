d3a97eb6e339dafc893f47984ac3d105e1738062
Luke David Turnbull <lukedturnbull@gmail.com>
2024-11-27 19:47:08 +1100

    Activity 3 - moved video-search out of it's own component and into the page

--- activity-3/src/app/favs/search/page.tsx
+++ activity-3/src/app/favs/search/page.tsx
@@ -1,7 +1,36 @@
 "use client";
 
-import VideoSearch from "@/components/video-search";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { YouTubeVideo } from "@/types/youtube";
+import { fetchYouTubeVideos } from "@/utils/fetchYouTubeVideos";
+import { useState } from "react";
+import { useQuery } from "react-query";
 
 export default function Search() {
-  return <VideoSearch />;
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const { data, isLoading } = useQuery<YouTubeVideo[]>({
+    queryKey: ["youtubeVideos", inputValue],
+    queryFn: () => fetchYouTubeVideos(inputValue),
+    enabled: !!inputValue,
+  });
+
+  return (
+    <div>
+      <div className="flex gap-2">
+        <Input
+          placeholder="Search for new favs"
+          defaultValue={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      </div>
+      <ScrollArea className="h-[calc(100vh-10rem)]">
+        {isLoading && <div>Loading...</div>}
+        {data && (
+          <pre>{data.map((video) => video.snippet.title).join("\n")}</pre>
+        )}
+      </ScrollArea>
+    </div>
+  );
 }
