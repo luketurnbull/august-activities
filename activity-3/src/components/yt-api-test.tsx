88677301489e61d4211b1af2193d3a37316ddfce
Luke David Turnbull <lukedturnbull@gmail.com>
2024-11-27 16:59:10 +1100

    Activity 3 - fix type error to stop build error in Vercel

--- activity-3/src/components/yt-api-test.tsx
+++ activity-3/src/components/yt-api-test.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 
 export default function YtApiTest() {
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<unknown>(null);
   useEffect(() => {
     fetch(
       `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&q=surfing`
