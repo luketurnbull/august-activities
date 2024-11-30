c04c5e6d75e8ab2c1e574f67bc22ec74151620d9
Luke David Turnbull <lukedturnbull@gmail.com>
2024-11-30 11:22:09 +1100

    Activity 2 - made the hamburger toggle more visible
    - background clashes with the white text hard, but will leave it as it's the design.
    - will change the toggle to have background blue that i have made a secondary button style for

--- activity-2/script.js
+++ activity-2/script.js
@@ -1,5 +1,6 @@
 document
-  .querySelector(".mobile-menu-toggle")
+  .querySelector("#mobile-menu-toggle")
   .addEventListener("click", function () {
     document.querySelector(".header-menu").classList.toggle("active");
+    document.querySelector("#header-nav").classList.toggle("active");
   });
